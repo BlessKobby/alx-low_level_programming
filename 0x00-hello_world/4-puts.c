5ba27860abd2e288837975ba944f0fb83fe0b870
qo88i <amablebless@gmail.com>
2023-08-13 19:27:29 -0700

    Name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry Point for Program.
- * return: 0 - Program executed successfully.
+ * main - entry point for program.
+ *
+ * Return: 0 - Program executed successfully.
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
