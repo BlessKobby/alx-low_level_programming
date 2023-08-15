ccaca110326394463af453f3a6b6cdb9fde777a1
qo88i <amablebless@gmail.com>
2023-08-15 10:04:03 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * main - Entry point of program.
@@ -16,22 +15,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-=======
-#include <stdio.h>
-#include <main.h>
-/**
- * main - Entry point for Program.
- * Return: 0 - Program executed successfully.
- */
-int _putchar(char c);/* Declaration of the _putchar function */
-
-int main(void)
-{
-	const char *message = "_putchar";
-	for (int i = 0; message[i] != '\0'; 1++)
-	{
-		_putchar(message[i]);
-	}
-	return (0);
->>>>>>> a141d3aebbc88bbb4c65bb5e1a222c726772b123
 }
