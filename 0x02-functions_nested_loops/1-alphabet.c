fc41e5d2d02d9e35b0aad145d21ec3fd058e58e9
qo88i <amablebless@gmail.com>
2023-08-15 10:42:11 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - Print out the English alphabets.
+ * Return:void - Program executed successfully.
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z' ; ch++)
+_putchar(ch);
+_putchar('\n');
+}
