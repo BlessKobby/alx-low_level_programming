0f53039e8f56896043176459205f645c989e42a2
qo88i <amablebless@gmail.com>
2023-08-16 09:09:27 -0700

    Add_Values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Sums to values.
+ *
+ * @n1: First number to be summed.
+ * @n2: Second number to be summed.
+ *
+ * Return: Integer value.
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
