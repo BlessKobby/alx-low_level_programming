de7ced46fc9e578fc87b91020aed8f3772a2ff37
qo88i <amablebless@gmail.com>
2023-08-17 05:33:11 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point program to test for positive or negative.
+*
+* Return: 0 - Program executed successfully.
+*/
+
+int main(void)
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
+
